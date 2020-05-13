f2ab77ca71e0f5644849d489d3f6897a0729142f
oleksandra <oleksandra.tkachenko@cloudmade.com>
2020-05-14 00:52:02 +0300

    added default logging tag

--- data/src/main/java/com/tinkooladik/airqualityindex/data/Logging.kt
+++ data/src/main/java/com/tinkooladik/airqualityindex/data/Logging.kt
@@ -2,28 +2,29 @@ package com.tinkooladik.airqualityindex.data
 
 import timber.log.Timber
 
+private const val defaultTag: String = "kuskus"
 
-fun Any.log(message: String, throwable: Throwable? = null, tag: String? = null) {
+fun Any.log(message: String, throwable: Throwable? = null, tag: String? = defaultTag) {
     Timber.tag(tag ?: this.javaClass.simpleName).run {
         if (throwable == null) d(message)
         else d(throwable, message)
     }
 }
 
-fun Any.logError(message: String, throwable: Throwable? = null, tag: String? = null) {
+fun Any.logError(message: String, throwable: Throwable? = null, tag: String? = defaultTag) {
     Timber.tag(tag ?: this.javaClass.simpleName).run {
         if (throwable == null) e(message)
         else e(throwable, message)
     }
 }
 
-fun Any.logWarn(message: String, throwable: Throwable? = null, tag: String? = null) {
+fun Any.logWarn(message: String, throwable: Throwable? = null, tag: String? = defaultTag) {
     Timber.tag(tag ?: this.javaClass.simpleName).run {
         if (throwable == null) w(message)
         else w(throwable, message)
     }
 }
 
-fun Any.logInfo(message: String, tag: String? = null) {
+fun Any.logInfo(message: String, tag: String? = defaultTag) {
     Timber.tag(tag ?: this.javaClass.simpleName).i(message)
 }
